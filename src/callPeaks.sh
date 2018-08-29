3c8dcc7906dd4979ba8ee28dc158d7f47e0b4eb5
urrutiag <gene.urrutia@gmail.com>
2018-08-29 14:01:54 -0400

    updated vignette

--- src/callPeaks.sh
+++ src/callPeaks.sh
@@ -3,11 +3,10 @@
 #SBATCH --mem=32g
 #SBATCH --time=24:00:00
 
-while getopts s:o:g:l:q: opt; do
+while getopts s:o:l:q: opt; do
   case ${opt} in
     s ) sampleName=$OPTARG  ;;
     o ) outDir=$OPTARG ;;
-    g ) genomeBedtools=$OPTARG ;;
     l ) blacklistFile=$OPTARG ;;
     q ) genomeMacs2=$OPTARG
   esac
@@ -21,7 +20,6 @@ finalBams=($bamDir/**.bam)
 echo sampleName: $sampleName
 echo PeaksDir: $peaksDir
 echo bamDir: $bamDir
-echo genomeBedtools: $genomeBedtools
 echo blacklistFile: $blacklistFile
 echo genomeMacs2: $genomeMacs2
 echo first10Bams: ${finalBams[@]:0:10}
@@ -37,14 +35,4 @@ macs2 callpeak -t "${finalBams[@]}" \
 bedtools subtract -a $peaksDir/"$sampleName"_peaks.narrowPeak \
   -b $blacklistFile > \
   $peaksDir/"$sampleName"_peaks.blacklist_removed.narrowPeak
-
-#set start and end to summit
-# awk 'BEGIN{ OFS="\t" } { summit=$10;  print $1, $2+$10, $2+$10, $4, $5, $6, $7, $8, $9, $10 }' \
-#  $peaksDir/"$sampleName"_peaks.blacklist_removed.narrowPeak > \
-#  $peaksDir/"$sampleName".summit.narrowPeak
-
-#expand to +/- 250 from summit
-#bedtools slop -i $peaksDir/"$sampleName".summit.narrowPeak \
-#  -g $genomeBedtools \
-#  -b 250 > $peaksDir/"$sampleName".500.narrowPeak
 
